a365cee1ebdde742f7aa8c6d4f774e026abc6b4e
Dominik Sulik <72816824+MiniSek@users.noreply.github.com>
2022-04-01 15:47:58 +0200

    flutter deleted from this branch

--- flutter_demo/README.md
+++
@@ -1,8 +0,0 @@
-# flutter_demo
-
-Flutter demo
-
-## Resources
-
-### Navigation bar
-https://oflutter.com/create-a-sidebar-menu-in-flutter/
--- flutter_demo/android/app/src/main/java/wytwarzania/brak/flutter_demo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package wytwarzania.brak.flutter_demo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
