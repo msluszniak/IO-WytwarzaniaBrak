639a6b3e4af56bf35d9e59ee8199724881c409a8
Janix4000 <janix4000@gmail.com>
2022-04-09 20:35:47 +0200

    add storage class

---
+++ lib/storage/storage.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:floor/floor.dart';
+import 'package:flutter_demo/storage/dao/exercise_dao.dart';
+import '../models/exercise.dart';
+
+
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+
+part 'storage.g.dart'; // the generated code will be there
+
+@Database(version: 1, entities: [Exercise])
+abstract class Storage extends FloorDatabase {
+  ExerciseDao get exerciseDAO;
+}
