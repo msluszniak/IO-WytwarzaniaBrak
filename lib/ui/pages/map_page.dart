22e78c99b40257d7fd9f12e4bc70ab7f4734ae46
VenomSolo <karolikjakub@gmail.com>
2022-04-11 20:52:15 +0200

    Syncing with database, caching, adding new gyms, tags and non-ascii letters are not handled for now.

--- lib/ui/pages/map_page.dart
+++ lib/ui/pages/map_page.dart
@@ -1,14 +1,16 @@
-import 'dart:math';
 import 'dart:async';
 
 import 'package:flutter_demo/models/gym.dart';
 import 'package:flutter_demo/ui/widgets/cards/gym_card.dart';
+import 'package:flutter_demo/backend/database_connection.dart';
 
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+
+import '../widgets/cards/new_gym_card.dart';
 
 class MapPage extends StatefulWidget {
   static const routeName = '/map';
@@ -18,8 +20,6 @@ class MapPage extends StatefulWidget {
   @override
   _MapState createState() => _MapState();
 }
-
-
 
 class _MapState extends State<MapPage> {
   List gymList = [];
@@ -32,22 +32,33 @@ class _MapState extends State<MapPage> {
   late StreamController<double?> _centerCurrentLocationStreamController;
   final MapController controller = new MapController();
   bool hasPermissions = false;
+  bool inAddingMode = false;
+  bool inCreatingMode = false;
+
+  Future<List<Gym>> setupMarkers() async {
+    return getGymData();
+  }
 
   //Populate gym list
   @override
   void initState() {
     super.initState();
-    Random rand = new Random();
-    for (int i = 0; i < 10; i++) {
-      gymList.add(new Gym(
-          startLat + pow(-1, i) * rand.nextDouble() * diffLat,
-          startLng + pow(-1, i) * rand.nextDouble() * diffLng,
-          "Gym " + i.toString(),
-          "Description of gym " + i.toString(),
-          "Address " + i.toString()));
-    }
+    getGymData().then((result) => gymList = result);
+    // for (int i = 0; i < 10; i++) {
+    //   gymList.add(new Gym(
+    //       startLat + pow(-1, i) * rand.nextDouble() * diffLat,
+    //       startLng + pow(-1, i) * rand.nextDouble() * diffLng,
+    //       "Gym " + i.toString(),
+    //       "Description of gym " + i.toString(),
+    //       "Address " + i.toString()));
+    // }
     //debug
-    gymList.add(new Gym(37.428528, -122.087967, "Oakland gym", "Oakland gym description", "Oakland gym address"));
+    gymList.add(new Gym(
+        lat: 37.428528,
+        lng: -122.087967,
+        name: "Oakland gym",
+        description: "Oakland gym description",
+        address: "Oakland gym address"));
     _centerOnLocationUpdate = CenterOnLocationUpdate.always;
     _centerCurrentLocationStreamController = StreamController<double?>();
     _checkPermission();
@@ -66,80 +77,112 @@ class _MapState extends State<MapPage> {
         title: const Text('Map'),
       ),
       body: FlutterMap(
-        mapController: this.controller,
-        options: MapOptions(
-          center: LatLng(51.5, -0.09),
-          zoom: 13,
-          maxZoom: 19,
-          onPositionChanged: (MapPosition position, bool hasGesture) {
-            if (hasGesture) {
-              setState(
-                () => _centerOnLocationUpdate = CenterOnLocationUpdate.never,
-              );
-            }
-          },
-        ),
-        layers: [
-          MarkerLayerOptions(
-            markers: [],
+          mapController: this.controller,
+          options: MapOptions(
+            center: LatLng(51.5, -0.09),
+            zoom: 13,
+            maxZoom: 19,
+            onPositionChanged: (MapPosition position, bool hasGesture) {
+              if (hasGesture) {
+                setState(
+                  () => _centerOnLocationUpdate = CenterOnLocationUpdate.never,
+                );
+              }
+            },
           ),
-        ],
-        children: <Widget>[
-          TileLayerWidget(
-            options: TileLayerOptions(
-                urlTemplate:
-                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c']),
-          ),
-          if (hasPermissions)
-            LocationMarkerLayerWidget(
-              plugin: LocationMarkerPlugin(
-                centerCurrentLocationStream:
-                    _centerCurrentLocationStreamController.stream,
-                centerOnLocationUpdate: _centerOnLocationUpdate,
-              ),
+          layers: [
+            MarkerLayerOptions(
+              markers: [],
             ),
-          MarkerLayerWidget(
-            options: MarkerLayerOptions(
-              markers: List.generate(
-                gymList.length,
-                (index) => Marker(
-                  width: 80.0,
-                  height: 80.0,
-                  point: gymList[index].getLatLng(),
-                  builder: (ctx) => Container(
-                    child: IconButton(
-                      icon: Icon(Icons.location_on),
-                      onPressed: (){
-                        setState(() {
-                          this.controller.move(gymList[index].getLatLng(), this.controller.zoom);
-                        });
-                        showModalBottomSheet(
-                            backgroundColor: Colors.transparent,
-                            context: context,
-                            shape: RoundedRectangleBorder(
-                              borderRadius:  BorderRadius.vertical(
-                                top: Radius.circular(20)
-                              )
-                            ),
-                            builder: (BuildContext context) => GymCard(selectedGym: gymList[index])
-                        );
-                      },
+          ],
+          children: <Widget>[
+            TileLayerWidget(
+              options: TileLayerOptions(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c']),
+            ),
+            if (hasPermissions)
+              LocationMarkerLayerWidget(
+                plugin: LocationMarkerPlugin(
+                  centerCurrentLocationStream:
+                      _centerCurrentLocationStreamController.stream,
+                  centerOnLocationUpdate: _centerOnLocationUpdate,
+                ),
+              ),
+            MarkerLayerWidget(
+              options: MarkerLayerOptions(
+                markers: List.generate(
+                  gymList.length,
+                  (index) => Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: gymList[index].getLatLng(),
+                    builder: (ctx) => Container(
+                      child: IconButton(
+                        icon: Icon(Icons.location_on),
+                        onPressed: () {
+                          setState(() {
+                            this.controller.move(gymList[index].getLatLng(),
+                                this.controller.zoom);
+                          });
+                          showModalBottomSheet(
+                              context: context,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.vertical(
+                                      top: Radius.circular(20))),
+                              builder: (BuildContext context) =>
+                                  GymCard(selectedGym: gymList[index]));
+                        },
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
           ],
           nonRotatedChildren: <Widget>[
-          Positioned(
-            right: 20,
-            bottom: 20,
-            child: _buildLocalizationButton(),
-          )
-        ],
-      ),
+            Center(
+              child: Visibility(
+                  visible: inAddingMode,
+                  child: Icon(Icons.location_on, size: 40, color: Colors.red)),
+            ),
+            Positioned(
+              right: 20,
+              bottom: 20,
+              child: _buildLocalizationButton(),
+            ),
+            Positioned(
+              left: 20,
+              bottom: 20,
+              child: _buildAddButton(),
+            ),
+            Positioned(
+                left: 20,
+                bottom: 100,
+                child: Visibility(
+                  child: _buildCancelButton(),
+                  visible: this.inAddingMode,
+                )),
+            Container(
+                child: Visibility(
+              child: NewGymCard(
+                mapController: this.controller,
+                cancelCallback: () {
+                  setState(() {
+                    this.inCreatingMode = false;
+                  });
+                },
+                submitCallback: () {
+                  setState(() {
+                    this.inCreatingMode = false;
+                    this.inAddingMode = false;
+                  });
+                },
+              ),
+              visible: this.inCreatingMode,
+            ))
+          ]),
     );
   }
 
@@ -179,8 +222,7 @@ class _MapState extends State<MapPage> {
         }
         if (hasPermissions) {
           setState(
-                () =>
-            _centerOnLocationUpdate = CenterOnLocationUpdate.always,
+            () => _centerOnLocationUpdate = CenterOnLocationUpdate.always,
           );
           _centerCurrentLocationStreamController.add(18);
         }
@@ -188,6 +230,43 @@ class _MapState extends State<MapPage> {
       child: const Icon(
         Icons.my_location,
         color: Colors.white,
+      ),
+    );
+  }
+
+  FloatingActionButton _buildCancelButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        setState(() {
+          this.inAddingMode = false;
+        });
+      },
+      child: Icon(
+        Icons.clear,
+        color: Colors.red,
+      ),
+    );
+  }
+
+  FloatingActionButton _buildAddButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        setState(() {
+          if (this.inAddingMode) {
+            setState(() {
+              this.inCreatingMode = true;
+              this.inAddingMode = true;
+            });
+          } else {
+            setState(() {
+              this.inAddingMode = true;
+            });
+          }
+        });
+      },
+      child: Icon(
+        (inAddingMode) ? Icons.done : Icons.add,
+        color: Colors.green,
       ),
     );
   }
@@ -200,7 +279,9 @@ class _MapState extends State<MapPage> {
             child: Text(
                 'Please turn on localization and grant this app permissions to it.'),
           ),
-          SizedBox(width: 20,),
+          SizedBox(
+            width: 20,
+          ),
           Expanded(
             child: ElevatedButton(
               onPressed: openAppSettings,
